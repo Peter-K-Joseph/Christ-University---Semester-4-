7bd3c6259f612e1810592f682e6e8628811ef2f7
Peter K Joseph <peterkjoseph02@gmail.com>
2022-08-17 07:16:50 +0530

    Added exp3

--- Computer Networks/exp3.c
+++ Computer Networks/exp3.c
@@ -11,36 +11,50 @@ char getXORvalue(char divident, char divisor) {
 }
 
 int main() {
-    char string[100], divisor[4], quotient[50], reminder[5], buffer;
-    int err_pos, quotpos = 0;
+    char string[100], key[4], key_duplicate[4], temp[10], quotient[50], reminder[5], buffer;
+    int err_pos, quotpos = 0, length_key, length_msg;
     printf("Enter a string: ");
     fgets(string, 100, stdin);
     string[strlen(string) - 1] = '\0';
-    printf("Enter a divisor: ");
-    fgets(divisor, 4, stdin);
-    divisor[strlen(divisor) - 1] = '\0';
-    printf("Enter error position: ");
-    scanf("%s", &buffer);
-    scanf("%d", &err_pos);
+    printf("Enter a key: ");
+    fgets(key, 4, stdin);
+    key[strlen(key) - 1] = '\0';
+    length_key = strlen(key);
+    length_msg = strlen(string);
     if (strlen(string) < 4) {
         printf("Error: String is too short.\n");
         return 0;
     }
-    for (int i = 0 ; i < strlen(string)-3; i++) {
-        reminder[0] = getXORvalue(string[i], divisor[0]);
-        reminder[1] = getXORvalue(string[i+1], divisor[1]);
-        reminder[2] = getXORvalue(string[i+2], divisor[2]);
-        reminder[3] = getXORvalue(string[i+3], divisor[3]);
-        reminder[4] = '\0';
-        if (reminder[0] == '0') {
-            quotient[quotpos] = '0';
-        } else {
-            quotient[quotpos] = '1';
-        }
-        printf("%c%c%c%c ^ '%c'%c'%c'%c' = %s\n", string[i], string[i+1], string[i+2], string[i+3], divisor[0], divisor[1], divisor[2], divisor[3], reminder);
-        quotpos++;
+    strcpy(key_duplicate, key);
+    for (int i = 0; i < length_key-1; i++) {
+        string[length_msg+i] = '0';
     }
-    quotient[quotpos] = '\0';
-    printf("%s\n", reminder);
-    printf("%s\n", quotient);
+    for (int i = 0; i < length_key; i++) {
+        temp[i] = string[i];
+    }
+    for (int i = 0 ; i < length_msg; i++) {
+        quotient[i] = temp[0];
+        if (quotient[i] == '0') {
+            for (int j = 0; j < length_key; j++) {
+                key[j] = '0';
+            }
+        } else {
+            for (int j = 0; j < length_key; j++) {
+                key[j] = key_duplicate[j];
+            }
+        }
+        for (int j = length_key-1; j > 0; j--) {
+            if (temp[j] == key[j]) {
+                if (temp[j] == key[j]) {
+                    reminder[j-1] = '0';
+                } else {
+                    reminder[j-1] = '1';
+                }
+            }
+        }
+        reminder[length_key-1] = string[i+length_key];
+        strcpy(temp, reminder);
+    }
+    strcpy(reminder, temp);
+    printf("Quotient: %s\nReminder: %s\nData: %s%s", quotient, reminder, string, reminder);
 }
